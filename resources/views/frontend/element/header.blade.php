065ff7490ab05f83f65b27454168a067b04859fd
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2016-09-12 21:04:48 +0700

    jkhljljl

--- resources/views/frontend/element/header.blade.php
+++ resources/views/frontend/element/header.blade.php
@@ -61,7 +61,7 @@
     <ul>
       <li id="nav01"><a href="{{ route('frontend.index') }}" title="ホーム">ホーム</a></li>
       <li id="nav02"><a href="feature/index.html" title="強み">強み</a></li>
-      <li id="nav03"><a href="plan/index.html" title="プラン">プラン</a></li>
+      <li id="nav03"><a href="{{route('frontend.plan.index')}}" title="プラン">プラン</a></li>
       <li id="nav04"><a href="support/index.html" title="サポート">サポート</a></li>
       <li id="nav05"><a href="faq/index.html" title="FAQ">FAQ</a></li>
       <li id="nav06"><a href="{{ route('frontend.company.index') }}" title="会社案内">会社案内</a></li>
