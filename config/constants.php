91e8d70326d7f63839731a5e9deec4e0938bdd7a
dophu17 <dophu17@gmail.com>
2016-09-14 09:49:27 +0700

    update contact constants

--- config/constants.php
+++ config/constants.php
@@ -8,8 +8,8 @@ return [
 		'MAIL_TO_ADDRESS_MANAGER'				=> 'phuongnd360@gmail.com',
 
 		//MOVE
-		'SUBJECT_CONTACT_USER'					=> 'contact email',
-		'SUBJECT_CONTACT_MANAGER'				=> 'contact email',
+		'SUBJECT_CONTACT_USER'					=> '【チロロネット】お問合せいただきありがとうございました。',
+		'SUBJECT_CONTACT_MANAGER'				=> '【くるみサーバー】お問い合わせがありました（自動配信メール）',
 		'SUBJECT_MOVE_COJP_USER'				=> 'Subject move co.jp user email',
 		'SUBJECT_MOVE_COJP_MANAGER'				=> 'Subject move co.jp manage email',
 		'SUBJECT_MOVE_COM_USER'					=> 'Subject move com user email',
